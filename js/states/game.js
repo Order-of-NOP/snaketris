9076d3ce792c56a118706066808e771a71fece85
Yngwarr <k.yngvarr@gmail.com>
2018-02-08 17:19:24 +0700

    More new-game proof, yknow

--- js/states/game.js
+++ js/states/game.js
@@ -3,22 +3,6 @@
 // TODO handle pause
 states['game'] = {
 	create: () => {
-		// wanna do something useful on right click
-		document.querySelector('canvas').oncontextmenu
-			= function() { return false; };
-
-		grid = new Grid(SIZE.W, SIZE.H);
-		input = new Input();
-
-		game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-		game.input.onDown.add(() => {
-			if (game.scale.isFullScreen) {
-				game.scale.stopFullScreen();
-			} else {
-				game.scale.startFullScreen(false);
-			}
-		}, this);
-
 		// demo mode
 		let ts = [0,1,2,6,7,8,9,10];
 		for (let r = 0; r < grid.h; ++r) {
--- js/states/preload.js
+++ js/states/preload.js
@@ -6,6 +6,22 @@ states['preload'] = {
 			TILE_SIZE, TILE_SIZE);
 	},
 	create: () => {
+		// wanna do something useful on right click
+		document.querySelector('canvas').oncontextmenu
+			= function() { return false; };
+
+		grid = new Grid(SIZE.W, SIZE.H);
+		input = new Input();
+
+		game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
+		game.input.onDown.add(() => {
+			if (game.scale.isFullScreen) {
+				game.scale.stopFullScreen();
+			} else {
+				game.scale.startFullScreen(false);
+			}
+		}, this);
+
 		//game.state.start('menu');
 		game.state.start('game');
 	}
