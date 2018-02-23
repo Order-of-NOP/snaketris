0cc53c694490250fefd33c5f912fffea43bf0605
komnotmr <komnotmr@gmail.com>
2018-02-23 17:40:11 +0700

    ready state

--- js/states/ready.js
+++ js/states/ready.js
@@ -1,8 +1,22 @@
 /* states/ready.js */
 
 const READY_GUI = {
-	BTNS: []
+	BTNS: [],
+	LBL: [],
+	RD_LBL: []
 };
+
+const READY_FLGS = [ false, false ];
+const READY_LBLS = ['READY', 'NOT READY'];
+
+const LBL_RDY_STL = {
+    font: "bold 18px Arial",
+    fill: "#ccc"
+};
+
+const TIME_DELAY = 3000;
+
+let RDY_TIME = null;
 
 states['ready'] = {
 	create: () => {
@@ -10,10 +24,58 @@ states['ready'] = {
 			game.state.start('game');
 			game.paused = false;
 		}, 'Начать игру', BTN_STYLE, 100));
+		READY_GUI.LBL.push( game.add.text(100, 200, 
+			'Press any key for say \'READY\'', LBL_RDY_STL));
+		READY_GUI.LBL.push( game.add.text(400, 200, 
+				'Press any key for say \'READY\'', LBL_RDY_STL));
+		READY_GUI.RD_LBL.push( game.add.text(100, 240, 
+			READY_LBLS[1], LBL_RDY_STL));
+		READY_GUI.RD_LBL.push( game.add.text(400, 240, 
+			READY_LBLS[1], LBL_RDY_STL));
 	},
 	update: () => {
+		if (RDY_TIME == null) {
+			for (let i = 0; i < 2; i++) {
+				let ip = input.p[i];
+				if (!READY_FLGS[i]) {
+					if (ip['left'].justReleased) {READY_FLGS[i] = !READY_FLGS[i];}
+					if (ip['down'].justReleased) { READY_FLGS[i] = !READY_FLGS[i]; }
+					if (ip['right'].justReleased) { READY_FLGS[i] = !READY_FLGS[i]; }
+					if (ip['up'].justReleased) { READY_FLGS[i] = !READY_FLGS[i]; }
+					
+					if (READY_FLGS[i]) {
+						READY_GUI.RD_LBL[i].text = READY_LBLS[0];
+					} else {
+						READY_GUI.RD_LBL[i].text = READY_LBLS[1];
+					}
+				}
+			}
+		}
+		if (RDY_TIME == null) {
+			if (READY_FLGS[0] && READY_FLGS[1]) {
+				console.log('READY!!!');
+				RDY_TIME = game.time.now + TIME_DELAY;
+			}
+		} else {
+			let time = RDY_TIME - game.time.now;
+			console.log(time);
+			if (time < 0) {
+				console.log(time);
+				game.state.start('game');
+			}
+		}
 	},
 	shutdown: () => {
 		LAST_GAME_STATE = 'ready';
+		// Здесь всё занулять и чистить
+		_.each(READY_GUI.BTNS, (e)=>{ e.destroy(); });
+		_.each(READY_GUI.LBL, (e)=>{ e.destroy(); });
+		_.each(READY_GUI.RD_LBL, (e)=>{ e.destroy(); });
+		READY_GUI.BTNS = [];
+		READY_GUI.LBL = [];
+		READY_GUI.RD_LBL = [];
+		READY_FLGS[0] = false;
+		READY_FLGS[1] = false;
+		RDY_TIME = null;
 	}
 };
