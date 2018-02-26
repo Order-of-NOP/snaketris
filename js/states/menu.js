cb2a0fc7c38255893b8a552fea403d497f3452fd
komnotmr <komnotmr@gmail.com>
2018-02-26 21:14:02 +0700

    fast fix

--- js/states/menu.js
+++ js/states/menu.js
@@ -5,18 +5,10 @@ const MENU = new Gui();
 
 states['menu'] = {
 	create: () => {
-		MENU.add_btn(
-			()=>{ game.state.start('ready'); }, 'Start game', TXT_STL.BTN);
-
-		MENU.add_btn(
-			()=>{ game.state.start('settings'); }, 'Settings', TXT_STL.BTN);
-		
-		MENU.add_btn(
-			()=>{ game.state.start('records'); }, 'Records', TXT_STL.BTN);	
-		
-		MENU.add_btn(
-			()=>{ game.state.start('gameover'); }, 'Game over', TXT_STL.BTN);	
-	
+		let st = state => game.state.start(state);
+		MENU.add_btn(()=>{ st('ready'); }, 'Start game', TXT_STL.BTN);
+		//MENU.add_btn(()=>{ st('settings'); }, 'Settings', TXT_STL.BTN);
+		MENU.add_btn(()=>{ st('records'); }, 'Records', TXT_STL.BTN);		
 	},
 	update: () => {
 		MENU.btn_choose();
